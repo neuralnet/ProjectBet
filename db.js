41770799b942338864caf8dc28347bf33a0ca406
Peanut <phucthaihong@gmail.com>
2015-10-20 00:59:16 +1100

    Change mysql pw to 123

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var Bookshelf = require('bookshelf');
 var config = {
    host: 'localhost',  // your host
    user: 'root', // your database user
-   password: '181986', // your database password
+   password: '1234', // your database password
    database: 'dbUsers',
    charset: 'UTF8_GENERAL_CI'
 };
