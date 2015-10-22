01ef182904d35d3dc52b80531a1236123028cf94
Peanut <phucthaihong@gmail.com>
2015-10-22 14:38:29 +1100

    Fix model.js

--- model.js
+++ model.js
@@ -1,10 +1,38 @@
 var DB = require('./db').DB;
 
 var User = DB.Model.extend({
-   tableName: 'Users',
-   idAttribute: 'userId',
+	tableName: 'Users',
+	idAttribute: 'userId',
+});
+
+var Bet = DB.Model.extend({
+	tableName: 'Bets',
+	idAttribute: 'betId'
+});
+
+var Match = DB.Model.extend({
+	tableName: 'Matchs',
+	idAttribute: 'matchId'
+});
+
+var Request = DB.Model.extend({
+	tableName: 'Requests',
+	idAttribute: 'requestId'
+});
+
+var UserFriend = DB.Model.extend({
+	tableName: 'UserFriendList',
+});
+
+var PlayerBet = DB.Model.extend({
+	tableName: 'PlayerBetList',
 });
 
 module.exports = {
-   User: User
+   User: User,
+   Bet: Bet,
+   Match: Match,
+   Request: Request,
+   UserFriend: UserFriend,
+   PlayerBet: PlayerBet
 };
